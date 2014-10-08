4fd777c3fab14aebffc395b2c6525629b2d71758
root <root@raspberrypi.(none)>
2014-10-08 14:30:34 +0900

    扱える数字の上限の緩和

--- merge_sort.c
+++ merge_sort.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <limits.h>
 
 #define data_num 100000
 
@@ -17,8 +18,8 @@ void Merge(int data[data_num],int left,int mid,int right){
 	for(j=0;j<n2;j++){
 		R[j]=data[mid+j];
 	}
-	L[i]=10000000;
-	R[j]=10000000;
+	L[i]=INT_MAX;
+	R[j]=INT_MAX;
 	i=0;
 	j=0;
 	for(k=left;k<right;k++){
@@ -50,7 +51,7 @@ int main(){
 	
 	int data[data_num];
 	for(i=0;i<data_num;i++){
-		data[i]=rand()%10000;
+		data[i]=(int)(rand()%INT_MAX);
 	}
 	
 	for(i=0;i<data_num;i++){
